7d469aa65863bee3f53d32b1ea9ae00ebc5aa4bb
Juanjo Bazán <jjbazan@gmail.com>
2019-08-12 09:51:01 +0200

    add sandbox

---
+++ sandbox/variable_planning.py
@@ -0,0 +1,7 @@
+# len(R) = 1
+
+M_vir # virial mass of the dark matter halo. In [5e10, 1e13] M_sun
+M_D # barionic disc mass. In [1.25e8, 5.3e11] M_sun
+M_tot(R) # dynamical mass of the galaxy
+M_D(R) # predicted disk mass at present time
+tau(R) # collapse time-scale
--- src/galactica/main_initial.py
+++ src/galactica/main_initial.py
@@ -1,8 +1,10 @@
 import math
 from . import halo, disk
 
+
 halo_virial_mass = 1e12  # [5e10, 1e13] Msol
-disk_mass =1e11         # [1.25e8, 5.3e11] Msol
+disk_mass = 1e11  # [1.25e8, 5.3e11] Msol
+max_radio_kpc = 16
 
 
 def total_mass(radio_kpc):
